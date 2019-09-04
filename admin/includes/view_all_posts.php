5f6a045ebbcc1fbd75a2b17635c747d3d7d3c99e
Michael Neely <michael@michaelneely.net>
2019-09-04 00:30:14 -0400

    Finished the 'author related posts' feature

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -86,6 +86,7 @@
             <th>View</th>
             <th>Edit</th>
             <th>Delete</th>
+            <th>Post Views</th>
         </tr>
     </thead>
     
@@ -105,6 +106,7 @@
             $post_tags = $row['post_tags'];
             $post_comment_count = $row['post_comment_count'];
             $post_date = $row['post_date'];
+            $post_views_count = $row['post_views_count'];
 
             echo "<tr>";
             ?>
@@ -132,6 +134,7 @@
             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
             echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this post?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
+            echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</td></a>";
             echo "</tr>";
         
         }
@@ -148,6 +151,14 @@
 
         $query = "DELETE FROM posts WHERE post_id = $the_post_id ";
         $delete_query = mysqli_query($connection, $query);
+        header("Location: posts.php");
+    }
+
+    if(isset($_GET['reset'])) {
+        $the_post_id = $_GET['reset'];
+
+        $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+        $reset_query = mysqli_query($connection, $query);
         header("Location: posts.php");
     }
 
--- post.php
+++ post.php
@@ -19,7 +19,14 @@
             if(isset($_GET['p_id'])) {
 
                 $the_post_id = $_GET['p_id'];
-            }
+
+                $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
+                $send_query = mysqli_query($connection, $view_query);
+
+                if(!$send_query) {
+                    die("Query failed" . mysqli_error($connection));
+                }
+            
             
             $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
             $select_all_posts = mysqli_query($connection, $query);
@@ -54,7 +61,17 @@
 
                 <hr>    
 
-                <?php } ?>
+                <?php }
+            
+            } else {
+                header("Location: index.php");
+            }
+            
+            
+            
+            
+            
+            ?>
 
                 <!-- Blog Comments -->
 
