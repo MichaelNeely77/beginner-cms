e6a12bb4c3bba31a0317b2d4df0445c7542ce377
Michael Neely <michael@michaelneely.net>
2019-09-22 23:48:53 -0400

    More escaping data and protecting from SQL injection

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,12 +2,12 @@
 
 
 if(isset($_POST['create_post'])) {
-    $post_title = $_POST['post_title'];
-    $post_user = $_POST['post_user'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
+    $post_title = escape($_POST['post_title']);
+    $post_user = escape($_POST['post_user']);
+    $post_category_id = escape($_POST['post_category']);
+    $post_status = escape($_POST['post_status']);
 
-    $post_image = $_FILES['post_image']['name'];
+    $post_image = escape($_FILES['post_image']['name']);
     $post_image_temp = $_FILES['post_image']['tmp_name'];
 
     $post_tags = $_POST['post_tags'];
