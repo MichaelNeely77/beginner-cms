307f5b68410b4b968d78be12538980927c85a0a6
Michael Neely <michael@michaelneely.net>
2019-09-28 00:10:37 -0400

    Finished 'delete posts' modal

--- admin/includes/delete_modal.php
+++ admin/includes/delete_modal.php
@@ -15,7 +15,7 @@
         <h3>Are you sure you want to delete this post?</h3>
       </div>
       <div class="modal-footer">
-      <a href="" class="btn btn-danger">Delete</a>
+      <a href="" class="btn btn-danger modal_delete_link">Delete</a>
         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
       </div>
     </div>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -153,7 +153,7 @@ include("delete_modal.php");
             echo "<td>$post_date</td>";
             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-            echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
+            echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
             // echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this post?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
             echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</td></a>";
             echo "</tr>";
@@ -191,8 +191,12 @@ include("delete_modal.php");
     $(document).ready(function(){
         $(".delete_link").on('click', function(){
             var id = $(this).attr("rel");
+            var delete_url = "posts.php?delete="+ id +" ";
 
-            alert(id);
+            $(".modal_delete_link").attr("href",delete_url);
+
+            $("#myModal").modal('show');
+
         });
     });
 
