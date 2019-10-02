4ac8c52399276f813342ca6bd7a2da30edd2cbf1
Michael Neely <michael@michaelneely.net>
2019-10-02 00:15:40 -0400

    Added logout form and 'Logged in as ' capabilities.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,23 +6,33 @@
 <!-- Login -->
 <div class="well">
 
+    <?php if(isset($_SESSION['user_role'])): ?>
 
+        <h4>Logged in as <?php echo $_SESSION['username'] ?></h4>
+
+        <a class="btn btn-primary" href="includes/logout.php">Logout</a>
+
+    <?php else :?>
     <h4>Login</h4>
 
-    <form action="includes/login.php" method="post">
-    <div class="form-group">
-        <input name="username" type="text" class="form-control" placeholder="Enter Username">
-        
-    </div>
-    <div class="input-group">
-        
-        <input name="password" type="password" class="form-control" placeholder="Enter Password">
-        <span class="input-group-btn">
-            <button class="btn btn-primary" name="login" type="submit">Submit</button>
-        </span>
-    </div>
-    </form>
-    <!-- /.input-group -->
+<form action="includes/login.php" method="post">
+<div class="form-group">
+    <input name="username" type="text" class="form-control" placeholder="Enter Username">
+    
+</div>
+<div class="input-group">
+    
+    <input name="password" type="password" class="form-control" placeholder="Enter Password">
+    <span class="input-group-btn">
+        <button class="btn btn-primary" name="login" type="submit">Submit</button>
+    </span>
+</div>
+</form>
+<!-- /.input-group -->
+
+<?php endif; ?>
+
+
 </div>
 
 
