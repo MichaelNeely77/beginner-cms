2f50367c5bb1d8a8855d1bff31db8a8eafd46c29
Michael Neely <michael@michaelneely.net>
2019-10-07 00:24:12 -0400

    Added further validation to forms

--- admin/functions.php
+++ admin/functions.php
@@ -198,12 +198,10 @@ function redirect($location) {
 function register_user($username, $email, $password) {
     global $connection;
 
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+
 
     if(username_exists($username)){
-        $message = "Username already exists.";
+
     }
 
     if(!empty($username) && !empty($email) && !empty($password)) {
@@ -223,4 +221,47 @@ function register_user($username, $email, $password) {
         
         }
         
+}
+
+function login_user($username, $email) {
+
+    global $connection;
+
+    $username = trim($username);
+    $password = trim($password);
+   
+$username = mysqli_real_escape_string($connection, $username );
+$password = mysqli_real_escape_string($connection, $password );
+
+$query = "SELECT * FROM users WHERE username = '{$username}' ";
+$select_user_query =  mysqli_query($connection, $query);
+    if(!$select_user_query) {
+        die("QUERY FAILED: " . mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($select_user_query)) {
+        $db_user_id = $row['user_id'];
+        $db_username = $row['username'];
+        $db_user_password = $row['user_password'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role = $row['user_role'];
+    }
+
+    // $password = crypt($password, $db_user_password);
+
+
+    if (password_verify($password, $db_user_password)) {
+
+        $_SESSION['username'] = $db_username;
+        $_SESSION['firstname'] = $db_firstname;
+        $_SESSION['lastname'] = $db_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+
+
+        header("Location: ../admin" );
+    } else {
+        header("Location: ../index.php");
+    }
+
 }
--- registration.php
+++ registration.php
@@ -9,48 +9,48 @@
     <?php 
         if(isset($_POST['submit'])) {
 
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
+            $username = trim($_POST['username']);
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
 
-            if(username_exists($username)){
-                $message = "Username already exists.";
+            $error = [
+                'username' => '',
+                'email' => '',
+                'password' => ''
+            ];
+
+            if(strlen($username) > 4) {
+                $error['username'] = 'Username needs to be at least 4 characters';
             }
 
-            if(!empty($username) && !empty($email) && !empty($password)) {
+            if($username == '') {
+                $error['username'] = 'Username cannot be empty';
+            }
 
-                $username = mysqli_real_escape_string($connection, $username);
-                $email = mysqli_real_escape_string($connection, $email);
-                $password = mysqli_real_escape_string($connection, $password);
+            if(username_exists($username)) {
+                $error['username'] = 'Username already exists. Choose another.';
+            }
 
-                $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
-        
-                // $query = "SELECT randSalt FROM users";
-                // $select_randsalt_query = mysqli_query($connection, $query);
-        
-                // if(!$select_randsalt_query) {
-                //     die("Query failed" . mysqli_error($connection));
-                // }
-        
-                // $row = mysqli_fetch_array($select_randsalt_query);
-        
-                // $salt = $row['randSalt'];
-                // $password =crypt($password, $salt);
-        
-                $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
-                $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber' )";
-                $register_user_query = mysqli_query($connection, $query);
-                if(!$register_user_query) {
-                    die("QUERY FAILED" . mysqli_error($connection) . ' ' . mysqli_errno($connection));
+            if($email == '') {
+                $error['email'] = 'Username cannot be empty';
+            }
+
+            if(email_exists($email)) {
+                $error['email'] = 'Username already exists. <a href="index.php>Please login</a>.';
+            }
+
+            if($password == '') {
+                $error['password'] = 'Password cannot be empty';
+            }
+
+            foreach($error as $key => $value) {
+                if(empty($value)) {
+                    register_user($username, $email, $password);
+
+                    login_user($username, $password);
                 }
-                
-                $message = "Your registration has been submitted";
-            } else {
-                $message = "Fields cannot be empty";
             }
-        } else {
-            $message = "";
-        }
+        } 
     
     ?>
     
@@ -65,7 +65,7 @@
                 <div class="form-wrap">
                 <h1>Register</h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                    <h6 class="text-center"><?php echo $message ?></h6>
+
                         <div class="form-group">
                             <label for="username" class="sr-only">username</label>
                             <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
