8d39c7e6b28079d3150df5ab23bdce189dc05a76
Michael Neely <michael@michaelneely.net>
2019-10-13 22:57:00 -0400

    Adding prepared statements to update categories function

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -7,6 +7,8 @@
                                     if(isset($_GET['edit'])) {
                                         $cat_id = $_GET['edit'];
 
+                                        
+
                                         $query = "SELECT * FROM categories WHERE cat_id = $cat_id ";
                                         $select_categories_id = mysqli_query($connection, $query);
     
@@ -26,12 +28,16 @@
                                         if(isset($_POST['update_category'])) {
                                             $the_cat_title = $_POST['cat_title'];
 
-                                        $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
-                                            $update_query = mysqli_query($connection, $query);
+                                            $stmt = mysqli_prepare($connection, "UPDATE categories SET cat_title = ? WHERE cat_id = ?");
 
-                                            if(!$update_query) {
+                                            mysqli_stmt_bind_param($stmt, 'si', $the_cat_title, $cat_id);
+                                            mysqli_stmt_execute($stmt);
+
+                                            if(!$stmt) {
                                                 die('QUERY FAILED' . mysqli_error($connection));
                                             }
+
+                                            redirect("categories.php");
 
                                         }
                                     ?>
